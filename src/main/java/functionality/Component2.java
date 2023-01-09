45ac9c7146b8b94d22867bec74268c8a7b2dfd4e
AnceTetere <ance.tetere@outlook.com>
2023-01-09 04:33:58 +0200

    Adding the component2

---
+++ src/main/java/functionality/Component2.java
@@ -0,0 +1,17 @@
+package functionality;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class Component2 extends JComponent implements MouseMotionListener {
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
