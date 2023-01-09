5863dd1b8e46e02b57d8c189f9b61f77c61fd5fc
AnceTetere <ance.tetere@outlook.com>
2023-01-09 03:02:02 +0200

    Adding a Car product.

---
+++ src/main/java/products/Car.java
@@ -0,0 +1,37 @@
+package products;
+
+public class Car {
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String colour = "Grey";
+    private int doors = 2;
+    private boolean convertible = true;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " +
+                colour + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
