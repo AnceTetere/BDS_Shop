4a4aad2c822aaf428e45fc075b0529ee86d526af
AnceTetere <ance.tetere@outlook.com>
2023-01-22 12:03:41 +0200

    Adding CustomerLimits class.

---
+++ src/main/java/functionality/CustomerLimits.java
@@ -0,0 +1,36 @@
+package functionality;
+
+public class CustomerLimits {
+
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+
+    public CustomerLimits() {
+        this("Nobody", "nobody@nowhere.com");
+    }
+
+    public CustomerLimits(String name, String email) {
+        this(name, 1000, email);
+        this.name = name;
+        this.emailAddress = email;
+    }
+
+    public CustomerLimits(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+}
